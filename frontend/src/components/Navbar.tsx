669c88b8dcd86ca14626cd209e57ed8ae1fa6b54
ivanst101 <ivanstojkovic177@gmail.com>
2025-10-05 20:17:56 +0200

    Navbar component with styles

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header className="max-w-6xl mx-auto p-5 flex justify-between items-center">
+      <h1 className="text-3xl underline  underline-offset-4">Noteword</h1>
+      <button className="bg-[var(--silver-lake-blue-color)] text-[var(--rich-black-color)] p-3 rounded-xl">
+        + Add note
+      </button>
+    </header>
+  );
+}
